6a262453c7c7788a5f940d5fbca64f992c908600
RainPPR <41764766+RainPPR@users.noreply.github.com>
2025-08-13 20:53:50 +0800

    Update

---
+++ script/main.py
@@ -0,0 +1,69 @@
+import json
+import common
+import requests
+from bs4 import BeautifulSoup
+from base64 import b64encode
+
+def write_markdown(data, description):
+    title = data['title']
+    author = data['author']
+    date = data['time']
+    content = data['content']
+    description = b64encode(description.encode('utf-8'))
+
+    print(f'Writing article: "{title}" by {author}...')
+
+    markdown = f'''---
+title: {title}
+date: {date}
+description: {description}
+author: {author}
+---
+
+{content}
+'''
+
+    file_path = f'./articles/{data["author"]}-{data["lid"]}.md'
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(markdown)
+
+    return file_path
+
+def fetch_article(lid, description=''):
+    url = f'https://www.luogu.com/article/{lid}'
+
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'html5lib')
+    script_tag = soup.find('script', id='lentille-context')
+
+    data = json.loads(script_tag.string)
+
+    data = data['data']['article']
+    data['author'] = data['author']['uid']
+    data['content'] = write_markdown(data, description)
+
+    return data
+
+def fetch_user_articles(uid):
+    article_list = []
+    for i in range(1, 999):
+        print(f'Fetching page {i} for user {uid}...')
+        data = common.fetch_json(f'https://www.luogu.com/api/article/find?user={uid}&page={i}')
+        if len(data['articles']['result']) == 0:
+            break
+        article_list += data['articles']['result']
+    print(f'Found {len(article_list)} articles for user {uid}.')
+    return [fetch_article(article['lid'], description=article['content']) for article in article_list]
+
+if __name__ == "__main__":
+    uid_list = common.load_json('uid.json')['uid_list']
+    all_articles = []
+    for uid in uid_list:
+        print(f'Fetching articles for user {uid}...')
+        user_articles = fetch_user_articles(uid)
+        all_articles += user_articles
+        common.write_json(f'./data/articles.{uid}.json', user_articles)
+    common.write_json(f'./data/articles.json', all_articles)
